e8385fa4243f31df7604674face76a1d1b3b4320
Sharat Chandra Janapareddy <sharat.chandraj@tamu.edu>
2020-11-15 20:29:21 -0600

    Implemented blocking device functions. Implemented Option 3 and 4

--- MP6_Sources/blocking_disk.C
+++ MP6_Sources/blocking_disk.C
@@ -1,10 +1,10 @@
 /*
      File        : blocking_disk.c
 
-     Author      : 
-     Modified    : 
+     Author      :
+     Modified    :
 
-     Description : 
+     Description :
 
 */
 
@@ -22,27 +22,55 @@
 #include "utils.H"
 #include "console.H"
 #include "blocking_disk.H"
+#include "scheduler.H"
+#include "thread.H"
 
+extern Scheduler * SYSTEM_SCHEDULER;
 /*--------------------------------------------------------------------------*/
 /* CONSTRUCTOR */
 /*--------------------------------------------------------------------------*/
 
-BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size) 
+BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size)
   : SimpleDisk(_disk_id, _size) {
+
+    bt_q_size = 0;
+    this->bt_queue =  new Queue();
 }
 
 /*--------------------------------------------------------------------------*/
 /* SIMPLE_DISK FUNCTIONS */
 /*--------------------------------------------------------------------------*/
 
-void BlockingDisk::read(unsigned long _block_no, unsigned char * _buf) {
-  // -- REPLACE THIS!!!
-  SimpleDisk::read(_block_no, _buf);
+
+void BlockingDisk::wait_until_ready() {
+
+    if (!SimpleDisk::is_ready()) {
+        Thread *current_thread = Thread::CurrentThread();
+        this->disk_enqueue(current_thread);
+        SYSTEM_SCHEDULER->yield();
+    }
 
 }
 
 
+void BlockingDisk::disk_enqueue(Thread *thread) {
+    this->bt_queue->enqueue(thread);
+    bt_q_size++;
+}
+
+//Since is ready is protected in SimpleDisk
+bool BlockingDisk::is_ready() {
+    return SimpleDisk::is_ready();
+}
+
+void BlockingDisk::read(unsigned long _block_no, unsigned char * _buf) {
+
+    SimpleDisk::read(_block_no, _buf);
+    Console::puts("END OF READ CALL!!!!!!!!!!!!!!!!!! \n");
+}
+
+
 void BlockingDisk::write(unsigned long _block_no, unsigned char * _buf) {
-  // -- REPLACE THIS!!!
-  SimpleDisk::write(_block_no, _buf);
+    SimpleDisk::write(_block_no, _buf);
+    Console::puts("END OF WRITE CALL!!!!!!!!!!!!!!!!!! \n");
 }
