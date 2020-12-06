c06b13b7f3c4ddbc0767d7c4bf3afba2d9ff2658
Sharat Chandra Janapareddy <sharat.chandraj@tamu.edu>
2020-12-06 16:02:48 -0600

    Implemented indexed file system

--- MP7_Sources/file.C
+++ MP7_Sources/file.C
@@ -26,11 +26,20 @@
 /* CONSTRUCTOR */
 /*--------------------------------------------------------------------------*/
 
-File::File() {
+File::File(Inode* _inode) {
     /* We will need some arguments for the constructor, maybe pointer to disk
      block with file management and allocation data. */
     Console::puts("In file constructor.\n");
-    assert(false);
+
+
+    inode = (Inode*) new Inode();
+
+    memcpy((unsigned char*)inode,(unsigned char*)_inode,sizeof(Inode));
+
+    file_block_index = 0;
+
+    //Console::puts("End of file constructor.\n");
+
 }
 
 /*--------------------------------------------------------------------------*/
@@ -39,28 +48,97 @@ File::File() {
 
 int File::Read(unsigned int _n, char * _buf) {
     Console::puts("reading from file\n");
-    assert(false);
+
+
+    unsigned int  chars_read = 0;
+    char data_buffer[512];
+    memset(data_buffer,0,512);
+
+    while(!EoF() && chars_read < _n)
+    {
+        FILE_SYSTEM->disk->read(inode->blocks[file_block_index++],(unsigned char*)data_buffer);
+
+        int remaining =_n-chars_read;
+
+        if(remaining>512)
+        {
+            memcpy(_buf+chars_read,data_buffer,512);
+            chars_read +=  512;
+        }
+        else
+        {
+            memcpy(_buf+chars_read,(char*)data_buffer,remaining);
+            chars_read += remaining;
+        }
+    }
+
+    Console::puts("No. of chars read: ");Console::puti(chars_read);Console::puts("\n");
+    return chars_read;
 }
 
 
 void File::Write(unsigned int _n, const char * _buf) {
     Console::puts("writing to file\n");
-    assert(false);
+
+
+    unsigned int chars_written = 0;
+
+    while(chars_written<_n)
+    {
+        int new_block = FILE_SYSTEM->get_empty_block();
+
+        FILE_SYSTEM->mark_used(new_block);
+
+        inode->blocks[inode->size++] = new_block;
+
+        char data_buffer[512];
+        memset(data_buffer,0,512);
+
+        int remaining = _n-chars_written;
+
+        if(remaining > 512)
+        {
+            memcpy(data_buffer,_buf,512);
+            FILE_SYSTEM->disk->write(new_block,(unsigned char*)data_buffer);
+            chars_written += 512;
+        }
+        else
+        {
+            memcpy(data_buffer,_buf,remaining);
+            FILE_SYSTEM->disk->write(new_block,(unsigned char*)data_buffer);
+            chars_written += remaining;
+        }
+        file_block_index++;
+    }
+
+    FILE_SYSTEM->disk->write(inode->unique_id,(unsigned char*)inode);
+
+    //Console::puts("Finished writing to file\n");
 }
 
 void File::Reset() {
     Console::puts("reset current position in file\n");
-    assert(false);
-    
+
+    file_block_index = 0;
+
 }
 
 void File::Rewrite() {
     Console::puts("erase content of file\n");
-    assert(false);
+
+    file_block_index = 0;
+
+    for(int i=0;i<inode->size;i++)
+    {
+        FILE_SYSTEM->mark_free(inode->blocks[i]);
+    }
+
+    inode->size = 0;
 }
 
 
 bool File::EoF() {
     Console::puts("testing end-of-file condition\n");
-    assert(false);
+
+    return (inode->size == 0 || ( inode->size < file_block_index));
 }
--- MP7_Sources/file_system.C
+++ MP7_Sources/file_system.C
@@ -12,7 +12,7 @@
 /* DEFINES */
 /*--------------------------------------------------------------------------*/
 
-    /* -- (none) -- */
+/* -- (none) -- */
 
 /*--------------------------------------------------------------------------*/
 /* INCLUDES */
@@ -27,36 +27,165 @@
 /* CONSTRUCTOR */
 /*--------------------------------------------------------------------------*/
 
-FileSystem::FileSystem() {
+unsigned int FileSystem::size;
+
+FileSystem::FileSystem()
+{
     Console::puts("In file system constructor.\n");
-    assert(false);
+    disk = NULL;
+    max_block_count=0;
 }
 
 /*--------------------------------------------------------------------------*/
 /* FILE SYSTEM FUNCTIONS */
 /*--------------------------------------------------------------------------*/
 
-bool FileSystem::Mount(SimpleDisk * _disk) {
+bool FileSystem::Mount(SimpleDisk * _disk)
+{
     Console::puts("mounting file system form disk\n");
-    assert(false);
+    disk = _disk;
+    max_block_count = (size/512);
+
+    //mark all the blocks
+    for(int i=0; i<10; i++)
+    {
+        inode_file_maps[i].file_id = -1;
+        inode_file_maps[i].unique_id = -1;
+    }
+
+    file_count = 0;
+    inode_file_maps_index = -1;
+    memset(block_bit_map,0,512);
+    return true;
 }
 
-bool FileSystem::Format(SimpleDisk * _disk, unsigned int _size) {
+bool FileSystem::Format(SimpleDisk * _disk, unsigned int _size)
+{
     Console::puts("formatting disk\n");
-    assert(false);
+
+    char data_buffer[512];
+    memset(data_buffer,0,512);
+
+    for(int i=0; i<512; i++)
+    {
+        _disk->write(i,(unsigned char*)data_buffer);
+    }
+
+    FileSystem::size = _size;
+
+    return true;
 }
 
-File * FileSystem::LookupFile(int _file_id) {
+File * FileSystem::LookupFile(int _file_id)
+{
     Console::puts("looking up file\n");
-    assert(false);
+
+
+    for(int i=0; i<10; i++)
+    {
+        if(inode_file_maps[i].file_id == _file_id)
+        {
+            int block_num = inode_file_maps[i].unique_id;
+
+            unsigned char data_buffer[512];
+            disk->read(block_num,data_buffer);
+
+            File* file = (File*) new File((Inode*)data_buffer);
+
+            return file;
+        }
+    }
+    return NULL;
 }
 
-bool FileSystem::CreateFile(int _file_id) {
+bool FileSystem::CreateFile(int _file_id)
+{
     Console::puts("creating file\n");
-    assert(false);
+
+    if(LookupFile(_file_id)!=NULL)
+        return false;
+
+    int new_block = get_empty_block();
+    unsigned char temp[512];
+
+    Inode* new_inode = (Inode*)temp;
+    new_inode->unique_id = new_block;
+    new_inode->size = 0;
+
+    mark_used(new_block);
+
+    disk->write(new_block,temp);
+
+    inode_file_maps_index = (++inode_file_maps_index)%10;
+    inode_file_maps[inode_file_maps_index].file_id = _file_id;
+    inode_file_maps[inode_file_maps_index].unique_id = new_block;
+    file_count++;
+
+    Console::puts("Created File!! \n");
+
+    return true;
 }
 
-bool FileSystem::DeleteFile(int _file_id) {
+bool FileSystem::DeleteFile(int _file_id)
+{
     Console::puts("deleting file\n");
-    assert(false);
+
+    File* f = LookupFile(_file_id);
+
+    if(f==NULL)
+        return false;
+
+
+    int block_num = f->inode->unique_id;
+
+    for(int i=0; i<f->inode->size; i++)
+    {
+        mark_free(f->inode->blocks[i]);
+    }
+
+    mark_free(block_num);
+
+    for(int i=0; i<10; i++)
+    {
+        if(inode_file_maps[i].file_id == _file_id)
+        {
+            inode_file_maps[i].file_id = -1;
+            inode_file_maps[i].unique_id = -1;
+        }
+    }
+
+    file_count--;
+
+    if(file_count==0)
+        inode_file_maps_index =-1;
+
+    Console::puts("Deleted File!! \n");
+
+    return true;
+}
+
+void FileSystem::mark_free(unsigned int block_num)
+{
+    block_bit_map[block_num/8] &= ~(1<<(block_num%8));
+}
+
+void FileSystem::mark_used(unsigned int block_num)
+{
+    block_bit_map[block_num/8] |= (1<<(block_num%8));
+}
+
+int FileSystem::get_empty_block()
+{
+    for(int index=0; index<max_block_count/8; index++)
+    {
+        for(int bit_no=0; bit_no<8; bit_no++)
+        {
+            if((block_bit_map[index] & (1<<bit_no)) == 0)
+            {
+                return (index*8+bit_no);
+            }
+        }
+    }
+
+    return -1;
 }
